9706491b996760fae679e4c1cb94a1a0215226f5
Nick Jantz <thenickjantz@gmail.com>
2023-12-10 22:08:56 -0600

    mobile styles

--- app/components/general/Step.js
+++ app/components/general/Step.js
@@ -10,43 +10,46 @@ export const Step = ({
 	handleClick
 }) => {
 	return (
-		<div onClick={(() => handleClick(icon))} className={classNames(
-			"step",
-			"center-flex",
-			"space-x-2",
-			"hover:cursor-pointer",
-			{ 'text-gray-500': icon !== formStep }
-		)}>
-			<div id="icon" className={classNames(
-				'center-flex',
-				'items-center',
-				'rounded-full',
-				'px-3',
-				'w-8',
-				'h-8',
-				{ 'text-white': icon === formStep },
-				{ 'bg-gray-100': icon !== formStep },
-				{ 'bg-orange-500': icon === formStep },
-				{ 'shadow-md': icon === formStep },
+		<>
+			<div onClick={(() => handleClick(icon))} className={classNames(
+				"step",
+				"center-flex",
+				"space-x-2",
+				'md:mx-3',
+				"hover:cursor-pointer",
+				{ 'text-gray-500': icon !== formStep }
 			)}>
-				<span className="material-symbols-outlined">{icon}</span>
-			</div>
-			<div id="step-info" className={classNames(
-					'flex',
-					'flex-col',
-					'font-bold',
-					'text-sm',
-					{ 'text-gray-500': icon !== formStep }
+				<div id="icon" className={classNames(
+					'center-flex',
+					'items-center',
+					'rounded-full',
+					'px-3',
+					'w-12',
+					'h-12',
+					{ 'text-white': icon === formStep },
+					{ 'bg-gray-100': icon !== formStep },
+					{ 'bg-orange-500': icon === formStep },
+					{ 'shadow-md': icon === formStep },
 				)}>
-				<span className={classNames(
-					{ 'text-orange-500': icon === formStep },
-					{ 'drop-shadow-sm': icon === formStep }
-				)}>Step {index + 1}</span>
-				<span className={classNames(
-					{ 'text-black': icon === formStep },
-					{ 'drop-shadow-sm': icon === formStep }
-				)}>{title}</span>
+					<span className="material-symbols-outlined">{icon}</span>
+				</div>
+				<div id="step-info" className={classNames(
+						'flex-col',
+						'font-bold',
+						'text-sm',
+						'hidden',
+						{ 'md:flex': icon === formStep }
+					)}>
+					<span className={classNames(
+						{ 'text-orange-500': icon === formStep },
+						{ 'drop-shadow-sm': icon === formStep },
+					)}>Step {index + 1}</span>
+					<span className={classNames(
+						{ 'text-black': icon === formStep },
+						{ 'drop-shadow-sm': icon === formStep },
+					)}>{title}</span>
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
--- app/components/pages/Sample.js
+++ app/components/pages/Sample.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import classNames from "classnames";
+
 import { PageWrapper } from '../utils/PageWrapper';
 import { Step } from '../general/Step';
 
@@ -17,7 +19,12 @@ export const Sample = () => {
 			description: 'Tell us about yourself.',
 			formInstructions: 'Please fill in your details so we could contact you.'
 		}, {
-			icon: 'contact_emergency',
+			icon: 'credit_card',
+			title: "Payment Info",
+			description: 'Payment information',
+			formInstructions: 'Please enter payment information.'
+		}, {
+			icon: 'health_and_safety',
 			title: 'Emergency Contact',
 			description: '',
 			formInstructions: ''
@@ -34,12 +41,24 @@ export const Sample = () => {
 			<h1 className='text-center'>Sample Signup Page</h1>
 			<div id="signup-form" className="my-8 max-w-[75%] rounded-3xl center-flex whole-shadow">
 				<div id="left" className="flex flex-col w-3/4 p-8">
-					<h2 className="text-center text-xl font-bold mb-8">Race Title</h2>
-					<div id="signup-step" className="center-flex space-x-4">
+					<h2 className="text-center text-xl font-bold mb-4 md:mb-8">Race Title</h2>
+					<div id="signup-step" className="center-flex mx-2">
 						{steps.map((item, index) => (
 							<Step {...item} index={index} handleClick={setFormStep} formStep={formStep} key={index} />
 						))}
 					</div>
+					{steps.map((item, index) => (
+						<div className={classNames(
+							'hidden',
+							'center-flex',
+							'font-bold',
+							'mt-4',
+							'md:hidden',
+							{ 'sm:flex': item.icon == formStep }
+						)} key={index}>
+							{item.title}
+						</div>
+					))}
 					<div id="form-title" className="">
 						<h3></h3>
 					</div>
@@ -47,7 +66,7 @@ export const Sample = () => {
 
 					</div>
 				</div>
-				<div id="right" className="w-1/4 rounded-r-3xl form-image-filter">
+				<div id="right" className="w-1/4 rounded-r-3xl hidden form-image-filter md:flex">
 
 				</div>
 			</div>
